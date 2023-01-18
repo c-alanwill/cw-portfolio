12d2bd231871059b374c48a11241e06f226c570a
Cory Williams <corywilliams@me.com>
2023-01-17 17:08:50 -0800

    Updates

---
+++ precache-manifest.d74e1792548f425c92a320ae01fba940.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c046ab99cb1e9f47c891",
+    "url": "/cw-portfolio/static/css/main.92d1f5a7.chunk.css"
+  },
+  {
+    "revision": "c046ab99cb1e9f47c891",
+    "url": "/cw-portfolio/static/js/main.c046ab99.chunk.js"
+  },
+  {
+    "revision": "db1a3d2522e1eac3faa6",
+    "url": "/cw-portfolio/static/css/1.06b800d9.chunk.css"
+  },
+  {
+    "revision": "db1a3d2522e1eac3faa6",
+    "url": "/cw-portfolio/static/js/1.db1a3d25.chunk.js"
+  },
+  {
+    "revision": "573fd2a0381b68c613eb",
+    "url": "/cw-portfolio/static/js/runtime~main.573fd2a0.js"
+  },
+  {
+    "revision": "765e877acfc2a22d2861e77135c50b75",
+    "url": "/cw-portfolio/index.html"
+  }
+];
