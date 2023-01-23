bac7050c11139be5fcbd21976b56e8104daa209c
Cory Williams <corywilliams@me.com>
2023-01-23 14:22:37 -0800

    Add icon to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,10 +2,16 @@ import React from 'react';
 
 function Footer() {
   return <footer className="d-flex justify-content-center my-5">
-  <a className="mx-3" href="https://www.linkedin.com/in/cory-williams-3150b714/"
-  ><i class="fa-brands fa-linkedin display-6"></i></a>
-  <a href="https://github.com/c-alanwill?tab=repositories">
-  <i class="fa-brands fa-github display-6"></i>
+  <a className="mx-4" href="https://www.linkedin.com/in/cory-williams-3150b714/">
+    <i className="fa-brands fa-linkedin display-6"></i>
+  </a>
+
+  <a className="mx-4" href="https://github.com/c-alanwill?tab=repositories">
+    <i className="fa-brands fa-github display-6"></i>
+  </a>
+  
+  <a className="mx-4" href="https://twitter.com/altrent123">
+    <i className="fa-brands fa-twitter display-6"></i>
   </a>
   </footer>
 }
