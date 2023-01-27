0cf881ebd00dba11722a1befa6e4960869b43cc9
Cory Williams <corywilliams@me.com>
2023-01-27 11:30:39 -0800

    Add links to Github for projects

--- src/components/pages/Card.js
+++ src/components/pages/Card.js
@@ -1,27 +1,29 @@
 import React from 'react';
 import '../../styles/Portfolio.css';
 import Projects from '../../projects.json';
-import { Card, Button, Form, Row, Col } from 'react-bootstrap';
+import { Card, Button, Col, CardGroup } from 'react-bootstrap';
 
 function PorfolioCard() {
   return (
     <>
+      <CardGroup>
       {Projects.map(((project) => (
-        // <p>{project.title}</p>
-        <Card style={{ width: '18rem' }}>
-            <a target="_blank" href={project.url}>
-            <Card.Img variant="top" src={project.image} />
-            </a>
-            <Card.Body>
-            <Card.Title>{project.title}</Card.Title>
-            <Card.Text>
-              {project.paragraph}
-            </Card.Text>
-            <Button href={project.repo}variant="primary">Github</Button>
-            </Card.Body>
-        </Card>
+        <Col xs={12} md={6} lg={4}>
+          <Card style={{ marginRight: "20px", marginBottom: '20px' }}>
+              <a target="_blank" href={project.url}>
+              <Card.Img variant="top" src={project.image} />
+              </a>
+              <Card.Body>
+              <Card.Title>{project.title}</Card.Title>
+              <Card.Text>
+                {project.paragraph}
+              </Card.Text>
+              <Button href={project.repo}variant="primary">Github Repository</Button>
+              </Card.Body>
+          </Card>
+        </Col>
       )))}
-    
+      </CardGroup>
     </>
   );
 }
--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -2,8 +2,8 @@ import React from 'react';
 import '../../styles/Main.css';
 import '../../styles/Portfolio.css';
 import PortfolioCard from './Card'
-// import { Container } from 'react-bootstrap';
-import { Row, Col } from 'react-bootstrap';
+import { Row, Col, CardGroup } from 'react-bootstrap';
+
 
 function Portfolio() {
   return (
@@ -19,49 +19,16 @@ function Portfolio() {
       {/* <Container>
         <PortfolioCard />
       </Container> */}
+
         <div className="row">
+        <CardGroup>
           <div className="img-container">
-              <PortfolioCard />
+            <PortfolioCard />
           </div>
+        </CardGroup>
         </div>
-  
-
-      {/* <a href="https://fuuko08.github.io/Group04-Translator-Comparison-Web-App/" 
-      id="translator" className="img-container">
-      <img className="my-5"src="./Assets/translator.png" alt="A translator app"/>
-      </a> */}
-
-
-        {/* <a href="https://github.com/c-alanwill?tab=repositories">
-        <i class="fa-brands fa-github display-6"></i>
-        </a> */}
-      
-      {/* <a href="https://bookaholicbootcampselina.herokuapp.com/"
-      id="bookaholics" className="img-container">
-      <img src="./Assets/bookaholics.png" alt="A stack of books representing bookaholics login page"/>
-      </a>
-
-      <a href="https://c-alanwill.github.io/coding-quiz/" 
-      id="translator" className="img-container">
-      <img className="my-5" src="./Assets/coding-quiz-challenge.png" alt="A coding-quiz-app"/>
-      </a>
-      
-      <a href="https://c-alanwill.github.io/Javascript-password-generator/"
-      id="bookaholics" className="img-container">
-      <img src="./Assets/password-generator.png" alt="A password generator app"/>
-      </a>
-
-      <a href="https://c-alan-note-taker.herokuapp.com/" 
-      id="translator" className="img-container">
-      <img className="my-5" src="./Assets/note-taker.png" alt="A note taking app"/>
-      </a>
-      
-      <a href="https://pwa-text-editor-cw.herokuapp.com/"
-      id="bookaholics" className="img-container">
-      <img src="./Assets/pwa-app.png" alt="A PWA app"/>
-      </a> */}
-
     </div>
+    
   );
 }
 
